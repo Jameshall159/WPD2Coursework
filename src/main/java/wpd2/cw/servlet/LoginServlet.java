7c7d0771dfe9d653be22ecdb5de8f0d68946d48b
Heather <heathz2222@gmail.com>
2018-04-17 11:24:51 +0100

    hi guys

--- src/main/java/wpd2/cw/servlet/LoginServlet.java
+++ src/main/java/wpd2/cw/servlet/LoginServlet.java
@@ -1,46 +1,79 @@
 package wpd2.cw.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import wpd2.cw.dbdemo.db.H2Person;
+import wpd2.cw.util.Mustache.H2Person;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import wpd2.cw.util.mustache.MustacheRender;
-import wpd2.cw.dbdemo.db.H2Person;
-import wpd2.cw.dbdemo.model.Person;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
+@Singleton
+public class LoginServlet extends BaseServlet {
+    @SuppressWarnings("unused")
+    static final Logger LOG = LoggerFactory.getLogger(LoginServlet.class);
 
-/**
- * Servlet for view all persons in the DB
- */
-public class LoginServlet extends HttpServlet {
+    private static final String LOGIN_TEMPLATE = "login.mustache";
+    private static final String EMPTY_FIELD_ERR = "Neither the user name or password can be empty";
+    private static final String BAD_PASSWORD_ERROR = "The password is incorrect";
+    private static final String ERR_PARAM = "err";
+    private static final String USER_NAME_PARAM = "userName";
 
-    private final H2Person h2Person;
-    private final MustacheRender mustache;
+    private H2Person h2Person;
 
+    @Inject
     public LoginServlet(H2Person h2Person) {
-        mustache = new MustacheRender();
         this.h2Person = h2Person;
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = UserFuncs.getCurrentUser(request);
 
-        String html = mustache.render("login.mustache", new PersonsClass());
+        Map<String,String> params = new HashMap<>();
+        params.put(USER_NAME_PARAM, userName);
 
-        // Set the content type and status code and render/write the response
-        response.setContentType("text/html");
-        response.setStatus(200);
-        response.getOutputStream().write(html.getBytes(Charset.forName("utf-8")));
+        showView(response, LOGIN_TEMPLATE, params);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = getString(request, UserFuncs.USERNAME_PARAMETER, "");
+        String password = getString(request, UserFuncs.USERPASS_PARAMETER, "");
 
-    private class PersonsClass {
-        List<Person> persons() {
-            return h2Person.findPersons();
+        Map<String,String> params = new HashMap<>();
+        params.put(USER_NAME_PARAM, name);
+
+        if (name.length() == 0 || password.length() == 0) {
+            // the user hasn't filled the form in properly, set errors are proceed
+            params.put(ERR_PARAM, EMPTY_FIELD_ERR);
+        } else if (!checkUserNameAndPassword(name, password)) {
+            // password is incorrect for user name
+            params.put(ERR_PARAM, BAD_PASSWORD_ERROR);
         }
+        if (params.containsKey(ERR_PARAM)) {
+            showView(response, LOGIN_TEMPLATE, params);
+        } else {
+            UserFuncs.setCurrentUser(request, name);
+            String targetURL = UserFuncs.getLoginRedirect(request);
+            response.sendRedirect(response.encodeRedirectURL(targetURL));
+        }
+    }
 
+    private boolean checkUserNameAndPassword(String userName, String password) {
+        if (h2Person.isRegistered(userName)) {
+            return h2Person.login(userName, password);
+        } else {
+            if (h2Person.register(userName, password)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
