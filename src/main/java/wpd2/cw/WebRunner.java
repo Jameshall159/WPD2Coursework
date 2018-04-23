9f9a47213ce768a4dd3a3841e71e0f7f8ba57f16
Heather <heathz2222@gmail.com>
2018-04-23 18:56:17 +0100

    8080

--- src/main/java/wpd2/cw/WebRunner.java
+++ src/main/java/wpd2/cw/WebRunner.java
@@ -1,27 +1,9 @@
-package wpd2.cw;// Copyright (c) 2018 Katrin Hartmann. All Rights Reserved.
-//
-// File:        WebRunner.java
-//
-// Copyright in the whole and every part of this source file belongs to
-// Cilogi (the Author) and may not be used, sold, licenced, 
-// transferred, copied or reproduced in whole or in part in 
-// any manner or form or in or on any media to any person other than 
-// in accordance with the terms of The Author's agreement
-// or otherwise without the prior written consent of The Author.  All
-// information contained in this source file is confidential information
-// belonging to The Author and as such may not be disclosed other
-// than in accordance with the terms of The Author's agreement, or
-// otherwise, without the prior written consent of The Author.  As
-// confidential information this source file must be kept fully and
-// effectively secure at all times.
-//
-
+package wpd2.cw;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class WebRunner {
     @SuppressWarnings("unused")
@@ -32,7 +14,7 @@ public class WebRunner {
     }
 
     public static void main(String[] args) throws Exception {
-        Server server = new Server(9001);
+        Server server = new Server(8080);
 
         String rootPath = WebRunner.class.getClassLoader().getResource(".").toString();
         WebAppContext webapp = new WebAppContext(rootPath + "webapp", "");
