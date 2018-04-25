f5797b931ae7b013f687ed612265627ff83fa23c
James Hall <james.hall.1995@hotmail.co.uk>
2018-04-25 12:13:50 +0100

    Removing

--- src/main/java/wpd2/cw/dbdemo/guice/RouteModule.java
+++ src/main/java/wpd2/cw/dbdemo/guice/RouteModule.java
@@ -1,24 +1,3 @@
-// Copyright (c) 2010 Tim Niblett All Rights Reserved.
-//
-// File:        RouteModule.java  (05-Oct-2010)
-// Author:      tim
-// $Id$
-//
-// Copyright in the whole and every part of this source file belongs to
-// Tim Niblett (the Author) and may not be used,
-// sold, licenced, transferred, copied or reproduced in whole or in
-// part in any manner or form or in or on any media to any person
-// other than in accordance with the terms of The Author's agreement
-// or otherwise without the prior written consent of The Author.  All
-// information contained in this source file is confidential information
-// belonging to The Author and as such may not be disclosed other
-// than in accordance with the terms of The Author's agreement, or
-// otherwise, without the prior written consent of The Author.  As
-// confidential information this source file must be kept fully and
-// effectively secure at all times.
-//
-
-
 package wpd2.cw.dbdemo.guice;
 
 import com.google.inject.servlet.ServletModule;
